fb2d09124caff500a0d21bb35184b5327c7ff578
Gabriel Marques Magalhães <gabriel.magalhaes.dev@gmail.com>
2025-06-24 00:29:32 -0300

    Add write_file function and morelorem.txt for file handling tests
    - Implemented a new function `write_file` in `functions/write_file.py` to handle file writing with directory validation.
    - Added a new text file `morelorem.txt` in `calculator/pkg/` containing sample text.
    - Updated `tests.py` to include tests for the `write_file` function, checking valid and invalid write operations.

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
-test = get_file_content("calculator", "main.py")
+test = write_file("calculator", "__pycache__", "wait, this isn't lorem ipsum")
 print(test)
-test = get_file_content("calculator", "pkg/calculator.py")
+test = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
 print(test)
-test = get_file_content("calculator", "/bin/cat")
+test = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
 print(test)
